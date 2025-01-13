978c52523c113d853820382f2ddf53b90952195b
Lavianzy <shevaihsan5@gmail.com>
2025-01-13 08:52:55 +0700

    added autofocus input

--- kasir/index.php
+++ kasir/index.php
@@ -43,7 +43,7 @@ if (isset($_SESSION['cart'])) {
         <div class="col-md-8">
             <form method="post" action="./keranjang_act.php">
                 <div class="form-group">
-					<input type="text" name="kode_barang" class="form-control" placeholder="Masukkan Kode Barang" autofocus>
+					<input type="text" name="kode_barang" id="kode_barcode" class="form-control" placeholder="Masukkan Kode Barang" autofocus>
                     <!-- <select name="id_barang" required>
                         <option>Pilih Barang</option>
                         <?php while ($row = mysqli_fetch_array($barang)) { ?>
@@ -94,42 +94,8 @@ if (isset($_SESSION['cart'])) {
 		</div>
 	</div>
 </div>
-<!-- <script type="text/javascript">
-
-	//inisialisasi inputan
-	var bayar = document.getElementById('bayar');
-
-	bayar.addEventListener('keyup', function (e) {
-        bayar.value = formatRupiah(this.value, 'Rp. ');
-        // harga = cleanRupiah(dengan_rupiah.value);
-        // calculate(harga,service.value);
-    });
-
-    //generate dari inputan angka menjadi format rupiah
-
-	function formatRupiah(angka, prefix) {
-        var number_string = angka.replace(/[^,\d]/g, '').toString(),
-            split = number_string.split(','),
-            sisa = split[0].length % 3,
-            rupiah = split[0].substr(0, sisa),
-            ribuan = split[0].substr(sisa).match(/\d{3}/gi);
-
-        if (ribuan) {
-            separator = sisa ? '.' : '';
-            rupiah += separator + ribuan.join('.');
-        }
-
-        rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-        return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
-    }
-
-    //generate dari inputan rupiah menjadi angka
-
-    function cleanRupiah(rupiah) {
-        var clean = rupiah.replace(/\D/g, '');
-        return clean;
-        // console.log(clean);
-    }
-</script> -->
+ <script type="text/javascript">
+	document.getElementById('kode_barang').focus();
+</script>
 </body>
 </html>
